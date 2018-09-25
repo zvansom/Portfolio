48d9230c00513e51c50935e7c365418e280635ef
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-09-25 16:33:59 -0700

    Renamed site title.  Added emailJS script tag.  Initial contact page.

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Contact = () => (
-  <div>
-    <h1>Contact PAGE</h1>
-    <p>Nihil saepe molestiae cupiditate facilis non dignissimos velit. Exercitationem et ut id eos architecto dolores unde omnis. Ex non voluptatem nam pariatur harum quis esse.</p>
-  </div>
-);
+// Look into EmailJS API
+// Look into reCATPTCHA
 
-export default Contact;
+export default class Contact extends Component {
+  state = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  handleChange = e => {
+   this.setState({
+     [e.target.name]: e.target.value,
+   })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('form submitted')
+  }
+
+  render() {
+    return (
+      <main>
+        <h2>Contact Me</h2>
+        <h3>Now all that's left is for me to get to know you!</h3>
+        <h4>Get in touch and let's build your little corner of the internet together.</h4>
+        <h5>You can find me on if you follow the links below, or fill out this form to get in touch with me.</h5>
+        <a href="#"><img src="" alt="Github" /></a>
+        <a href="#"><img src="" alt="Linked In" /></a>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="name" placeholder="name" value={this.state.name} onChange={this.handleChange} />
+          <input type="email" name="email" placeholder="email" value={this.state.email} onChange={this.handleChange} />
+          <textarea type="text" name="message" placeholder="message" value={this.state.message} onChange={this.handleChange}></textarea>
+          {/* RECAPTCHA HERE */}
+          <input type="submit" value="Submit" />
+        </form>
+      </main>
+    )
+  }
+}
