61f8622fa16554a12b2ab6e193f2b2940901ce93
Zach VanSomeren <zvansomeren@gmail.com>
2018-09-26 07:52:59 -0700

    Reconsidered use of form due to security concerns

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -1,27 +1,6 @@
 import React, { Component } from 'react';
 
-// Look into EmailJS API
-// Look into reCATPTCHA
-
 export default class Contact extends Component {
-  state = {
-    name: "",
-    email: "",
-    message: "",
-  }
-
-  handleChange = e => {
-   this.setState({
-     [e.target.name]: e.target.value,
-   })
-  }
-
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log('form submitted')
-    // Insert EmailJS call here.
-  }
-
   render() {
     return (
       <main>
@@ -31,13 +10,7 @@ export default class Contact extends Component {
         <h5>You can find me on if you follow the links below, or fill out this form to get in touch with me.</h5>
         <a href="#"><img src="" alt="Github" /></a>
         <a href="#"><img src="" alt="Linked In" /></a>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="name" placeholder="name" value={this.state.name} onChange={this.handleChange} />
-          <input type="email" name="email" placeholder="email" value={this.state.email} onChange={this.handleChange} />
-          <textarea type="text" name="message" placeholder="message" value={this.state.message} onChange={this.handleChange}></textarea>
-          {/* RECAPTCHA HERE */}
-          <input type="submit" value="Submit" />
-        </form>
+        <a href="mailto:zvansomeren@gmail.com">Email</a>
       </main>
     )
   }
