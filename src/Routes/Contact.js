cf1ab4e078430c775d01bdbb4a89f9f56b4db36b
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-10-02 17:15:27 -0700

    Add multiple assets

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -7,7 +7,7 @@ export default class Contact extends Component {
         <h2>Contact Me</h2>
         <h3>Now all that's left is for me to get to know you!</h3>
         <h4>Get in touch and let's build your little corner of the internet together.</h4>
-        <h5>You can find me on if you follow the links below, or fill out this form to get in touch with me.</h5>
+        <h5>You can reach me at any one of the links below.</h5>
         <a href="#"><img src="" alt="Github" /></a>
         <a href="#"><img src="" alt="Linked In" /></a>
         <a href="mailto:zvansomeren@gmail.com">Email</a>
