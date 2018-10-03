3745839486ce6da6b94d8ca0c5bd1bd39acbbc67
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-10-03 06:42:44 -0700

    Update projects page.

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const ProjectCard = props => (
-  <section>
-    <h3>{props.title}</h3>
-    <h4>{props.tagline}</h4>
-    <a href={props.link}><img src={props.imgSrc} alt={props.title} /></a>
-    <p>{props.description}</p>
+  <section className="project-card">
+    <h3 className="project-card--title">{props.title}</h3>
+    <h4 className="project-card--subtitle">{props.tagline}</h4>
+    <p className="project-card--description">{props.description}</p>
+    <a className="project-card--screenshot" href={props.link}>
+      <img src={props.imgSrc} alt={props.title} />
+    </a>
   </section>
 )
 
--- src/Routes/Projects.js
+++ src/Routes/Projects.js
@@ -5,14 +5,14 @@ const PROJECT_DATA = [
   {
     title: 'Pacman',
     tagline: 'A clone of a classic',
-    link: '#',
-    imgSrc: '#',
+    link: 'https://zvansom.github.io/Pacman/',
+    imgSrc: '../assets/pacman.png',
     description: 'My earliest memory of a computer game was this arcade classic.  This is my humble tribute to Pacman, Inky, Blinky, Pinky, and Clyde.  This project was built on the Phaser game engine and in Javascript.  It was a challenging and rewarding experience as I learned about collision detection, animating sprites, and state management.',
   },{
     title: 'Brewly',
     tagline: 'A homebrewers resource',
-    link: '#',
-    imgSrc: '#',
+    link: 'https://zvansom-brewly.herokuapp.com/',
+    imgSrc: '../assets/brewly.png',
     description: 'In my free time, my wife and I enjoy brewing our own beer.  We\'ve always used a pen and paper to record our notes on the process.  Now we use 1s and 0s.  This was my first full-stack project.  It follows the traditional MVC structure and relies on RESTful routing.  The server side application is written in Express in a Node environment.  It uses a Postgres relational database for persistant data.  The views are all rendered from embedded Javascript.',
   }
 ]
@@ -20,8 +20,8 @@ const PROJECT_DATA = [
 export default class Projects extends Component {
   render() {
     return (
-    <main>
-      <h2>Projects</h2>
+    <main className="page">
+      <h2 className="page--title">Projects</h2>
       {PROJECT_DATA.map(project => 
         <ProjectCard 
           title={project.title}
