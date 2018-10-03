92fc18b64ddb1132844eb38e574a1baa28b05e69
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-10-02 17:15:45 -0700

    Navbar refresh

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,52 +1,32 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import GitHub from '../assets/github.svg';
+import LinkedIn from '../assets/linkedin.svg';
+import Resume from '../assets/file.svg';
 
 export default class Navbar extends Component {
+  state = {
+    navOpen: false,
+  }
+
+  toggleClass = e => { this.setState({navOpen: !this.state.navOpen}) }
+
   render() {
-    const styles = {
-      background: '#eee',
-      color: '#222',
-      borderBottom: '2px solid #22222280',
-    }
     return(
-      <StyledNav>
-        <StyledLink activeStyle={{...styles}}  exact to="/">Home</StyledLink>
-        <StyledLink activeStyle={{...styles}} to="/about">About Me</StyledLink>
-        <StyledLink activeStyle={{...styles}} to="/resume">Resume</StyledLink>
-        <StyledLink activeStyle={{...styles}} to="/skills">Skills</StyledLink>
-        <StyledLink activeStyle={{...styles}} to="/projects">Projects</StyledLink>
-        <StyledLink activeStyle={{...styles}} to="/contact">Contact</StyledLink>
-      </StyledNav>
+      <nav>
+        <button onClick={ this.toggleClass }>X</button>
+        <div className="social-media">
+          <a className="resume" href="#"><img className="social-media__icons" src={Resume} />Resume</a>
+          <a href="https://github.com/zvansom"><img className="social-media__icons" src={GitHub} /></a>
+          <a href="https://www.linkedin.com/in/zach-vansomeren/"><img className="social-media__icons" src={LinkedIn} /></a>
+        </div>
+        <div className={this.state.navOpen ? "nav-drawer drawer-open" : "nav-drawer"}>
+          <NavLink exact to="/">Home</NavLink>
+          <NavLink to="/about">About Me</NavLink>
+          <NavLink to="/projects">Projects</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </div>
+      </nav>
     )
   }
 }
-
-const StyledNav = styled.nav`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, 1fr);
-  grid-auto-flow: column;
-  align-items: center;
-  background-color: #22222280;
-
-  > a:nth-of-type(1) {
-    margin-left: 100px;
-  }
-
-  > a:last-of-type {
-    margin-right: 100px;
-  }
-`;
-
-const StyledLink = styled(NavLink)`
-  color: white;
-  padding: 5px 0 0;
-  min-height: 30px;
-  font-size: 2rem;
-  text-decoration: none;
-  text-transform: uppercase;
-  transition: all 0.3s ease-in;
-  &:hover {
-    background: #444;
-  }
-`;
--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,34 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 
 const Home = () => (
-  <StyledMain>
+  <main className="home">
     <h2>Hi, I'm <span>Zach</span></h2>
     <h3>I like to build things</h3>
     <p>
       I've built things my whole life.  Now I build performative, engaging websites.  Let's build something together.
     </p>
-    <a className="button" href="/about">Step 1</a>
-  </StyledMain>
+    <a className="button" href="/about">About Me</a>
+  </main>
 );
 
 export default Home;
-
-const StyledMain = styled.main`
-  grid-template-rows: 1fr 2fr 3fr 1fr;
-  
-  > h2 {
-    align-self: flex-end;
-    font-size: 4rem;
-    border: 1px solid black;
-    > span {
-      text-transform: uppercase;
-    }
-  }
-  > h3 {
-    
-  }
-  > a {
-    justify-self: center;
-  }
-`;
