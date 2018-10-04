7385f682f72c9b889f212c5c8a64dbb487a0f34f
Zach VanSomeren <zachvansomeren@Zachs-MBP.local.tld>
2018-10-03 17:25:07 -0700

    SVGs loaded as components.  CSS cleanup

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,30 +3,47 @@ import { NavLink } from 'react-router-dom';
 import GitHub from '../assets/github.svg';
 import LinkedIn from '../assets/linkedin.svg';
 import Resume from '../assets/file.svg';
-import menu from '../assets/menu.svg';
-import close from '../assets/close.svg';
+import { ReactComponent as Menu } from '../assets/menu.svg';
+import { ReactComponent as Close } from '../assets/close.svg';
 
 export default class Navbar extends Component {
   state = {
     navOpen: false,
   }
 
-  toggleClass = e => { this.setState({navOpen: !this.state.navOpen}) }
+  toggleDrawer = e => { this.setState({navOpen: !this.state.navOpen}) }
 
   render() {
     return(
       <nav>
-        <img src={this.state.navOpen ? close : menu } onClick={ this.toggleClass } />
+        {this.state.navOpen ? 
+          <Close onClick={ this.toggleDrawer } className="nav-menu" /> :
+          <Menu onClick={ this.toggleDrawer } className="nav-menu" />
+        }
         <div className="social-media">
-          <a className="resume" href="#"><img className="social-media__icons" src={Resume} />Resume</a>
-          <a href="https://github.com/zvansom"><img className="social-media__icons" src={GitHub} /></a>
-          <a href="https://www.linkedin.com/in/zach-vansomeren/"><img className="social-media__icons" src={LinkedIn} /></a>
+          <a className="resume" href="#" target="_blank">
+            <img className="social-media__icons" src={Resume} alt="Zach VanSomeren's Resume" />Resume
+          </a>
+          <a href="https://github.com/zvansom">
+            <img className="social-media__icons" src={GitHub} alt="Github" />
+          </a>
+          <a href="https://www.linkedin.com/in/zach-vansomeren/">
+            <img className="social-media__icons" src={LinkedIn} alt="LinkedIn" />
+          </a>
         </div>
+
+        {/* --------------------------- */}
+        {/* ----       DRAWER      ---- */}
+        {/* --------------------------- */}
+
         <div className={this.state.navOpen ? "nav-drawer drawer-open" : "nav-drawer"}>
-          <NavLink activeClassName="selected" exact to="/">Home</NavLink>
-          <NavLink activeClassName="selected" to="/about">About Me</NavLink>
-          <NavLink activeClassName="selected" to="/projects">Projects</NavLink>
-          <NavLink activeClassName="selected" to="/contact">Contact</NavLink>
+          <NavLink activeClassName="selected" exact to="/" onClick={ this.toggleDrawer }>Home</NavLink>
+          <NavLink activeClassName="selected" to="/about" onClick={ this.toggleDrawer }>About Me</NavLink>
+          <NavLink activeClassName="selected" to="/projects" onClick={ this.toggleDrawer }>Projects</NavLink>
+          <NavLink activeClassName="selected" to="/contact" onClick={ this.toggleDrawer }>Contact</NavLink>
+          <div className="footer">
+            
+          </div>
         </div>
       </nav>
     )
