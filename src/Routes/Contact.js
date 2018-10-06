b8e85f6d701fd887c1d8749ea1f3e26441411270
Zach VanSomeren <zvansomeren@gmail.com>
2018-10-06 07:15:51 -0700

    Home styling and contact page content update

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 export default class Contact extends Component {
   render() {
     return (
-      <main>
+      <main className="page">
         <h2>Contact Me</h2>
         <h3>Now all that's left is for me to get to know you!</h3>
-        <h4>Get in touch and let's build your little corner of the internet together.</h4>
-        <h5>You can reach me at any one of the links below.</h5>
-        <a href="#"><img src="" alt="Github" /></a>
-        <a href="#"><img src="" alt="Linked In" /></a>
+        <a href="https://github.com/zvansom">
+          <img src="" alt="Github" />
+        </a>
+        <a href="https://www.linkedin.com/in/zach-vansomeren/">
+          <img src="" alt="Linked In" />
+        </a>
         <a href="mailto:zvansomeren@gmail.com">Email</a>
       </main>
     )
--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Home = () => (
-  <main className="home">
-    <h2>Hi, I'm <span>Zach</span></h2>
-    <h3>I like to build things</h3>
-    <p>
+  <main className="page">
+    <h2 className="home--title">Hi, I'm <span>Zach</span></h2>
+    <h3 className="home--subtitle">I like to build things</h3>
+    <p className="home--text">
       I've built things my whole life.  Now I build performative, engaging websites.  Let's build something together.
     </p>
-    <a className="button" href="/about">About Me</a>
+    <a className="home--cta" href="/about">About Me</a>
   </main>
 );
 
