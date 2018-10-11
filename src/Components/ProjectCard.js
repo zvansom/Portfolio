ad321a809d5923209a2c24aa52f5cf553f117e70
Zach VanSomeren <zvansomeren@gmail.com>
2018-10-10 19:15:52 -0700

    Awards update

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -9,9 +9,9 @@ const ProjectCard = ({title, tagline, description, github, link, imgSrc, award='
       <a className="project-card--link" href={github}>Github</a>
       {link && <a className="project-card--link" href={link}>Link</a>}
     </div>
-    <div>
+    <div className="image-container">
       <img className="project-card--screenshot" src={imgSrc} alt={title} />
-      <p>{award}</p>
+      <p className="project-card--awards">{award}</p>
     </div>
   </section>
 )
